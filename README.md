8f36c0de731bb302b7945881b803892c683fb7af
Joseph Abraham <ekurubal24@gmail.com>
2020-10-24 14:39:58 +0300

    added files and changed Readme

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# perfect-number
+# JS_Perfect-number
+PYTHON Codes  =>
+def perfect_verification(number):
+    dividers = [i for i in range(1,number) if number%i==0]
+    sum_dividers = 0
+    for i in dividers:
+        sum_dividers +=i
+    if sum_dividers == number: return "It's a perfect number"
+    else: return "It's not a perfect number"
---
+++ script.js
@@ -0,0 +1,27 @@
+document.getElementById("check").addEventListener(
+    "click", perfectVerification)
+
+
+    function perfectVerification(){
+        let number = document.getElementById("guess").value;
+
+        let dividers = [];
+        let sum_dividers = 0;
+
+        for (let i = 1; i< number; i++){
+            if (number % i == 0){
+                dividers.push(i)
+                sum_dividers+=i
+            };
+        }
+        if (sum_dividers == number) {
+            document.getElementById("answer").innerHTML = `${number} is a perfect number`;
+        } else {
+            document.getElementById("answer").innerHTML = `${number} is not a perfect number`;
+        }
+        document.getElementById("guess").value=""; 
+        document.getElementById("guess").focus();
+    }
+    
+
+    
